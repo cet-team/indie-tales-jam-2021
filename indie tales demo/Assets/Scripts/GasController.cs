458a69296d5a8dec30826abf5d9214ec5de149b0
Jakob <jakobmauthe@gmail.com>
2021-07-10 15:09:32 +0200

    Update
    Added smokeground
    smoketilemap
    gas controller

--- indie tales demo/Assets/Scripts/GasController.cs
+++ indie tales demo/Assets/Scripts/GasController.cs
@@ -6,23 +6,35 @@ using UnityEngine.Tilemaps;
 public class GasController : MonoBehaviour {
 
     [SerializeField] GameObject prefabSmoke;
-    [SerializeField] Tilemap smokeMap;
+    [SerializeField] Tilemap smokeMap;    
+    [SerializeField] TileBase smokeGroundTile;
+    Vector3 origin;
+    Vector3Int originAsInt;
 
     private void Start() {
         Invoke("StartLeaking", 1f);
+        origin = smokeMap.LocalToCellInterpolated(transform.position);
+        originAsInt = smokeMap.LocalToCell(transform.position);
     }
 
     private void StartLeaking() {
-        GameObject smokeObject = Instantiate(prefabSmoke, smokeMap.LocalToCellInterpolated(transform.position), Quaternion.identity);
+        GameObject smokeObject = Instantiate(prefabSmoke, origin , Quaternion.identity);
+        smokeMap.SetTile(originAsInt, smokeGroundTile);
+        //smokeMap.In
         /*
         Projectile projectile = projectileObject.GetComponent<Projectile>();
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
         audioSource.PlayOneShot(soundThrow);*/
-    }
+    }    
 
     private void LeakInAllPossibledirections() {
+        Vector3 Up = origin + Vector3.up;
+        Vector3 Down = origin + Vector3.down;
+        Vector3 Left = origin + Vector3.left;
+        Vector3 Right = origin + Vector3.right;
+
 
     }
 }
