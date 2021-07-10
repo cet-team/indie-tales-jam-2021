ae95c8303b9fb340aae4579262623a894b7acd92
Jakob <jakobmauthe@gmail.com>
2021-07-10 21:44:35 +0200

    Update
    Smoke halfway working

--- indie tales demo/Assets/Scripts/GasController.cs
+++ indie tales demo/Assets/Scripts/GasController.cs
@@ -8,34 +8,18 @@ public class GasController : MonoBehaviour {
     [SerializeField] GameObject prefabSmoke;
     [SerializeField] Tilemap smokeMap;    
     [SerializeField] TileBase smokeGroundTile;
+    [SerializeField] WaitForSeconds timeBetweenSpreading = new WaitForSeconds(1f);
     Vector3 origin;
     Vector3Int originAsInt;
 
     private void Start() {
-        Invoke("StartLeaking", 1f);
+        Invoke("StartLeaking", 2f);
         origin = smokeMap.LocalToCellInterpolated(transform.position);
         originAsInt = smokeMap.LocalToCell(transform.position);
     }
 
     private void StartLeaking() {
-        GameObject smokeObject = Instantiate(prefabSmoke, origin , Quaternion.identity);
+        Instantiate(prefabSmoke, origin , Quaternion.identity);
         smokeMap.SetTile(originAsInt, smokeGroundTile);
-        //smokeMap.In
-        /*
-        Projectile projectile = projectileObject.GetComponent<Projectile>();
-        projectile.Launch(lookDirection, 300);
-
-        animator.SetTrigger("Launch");
-        audioSource.PlayOneShot(soundThrow);*/
-    }    
-
-    private void LeakInAllPossibledirections(Vector3 origin) {
-        Vector3 Up = origin + Vector3.up;
-        Vector3 Down = origin + Vector3.down;
-        Vector3 Left = origin + Vector3.left;
-        Vector3 Right = origin + Vector3.right;
-
-
-
-    }
+    }       
 }
--- indie tales demo/Assets/Scripts/Smoke.cs
+++ indie tales demo/Assets/Scripts/Smoke.cs
@@ -2,10 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Smoke : MonoBehaviour
-{
-    private void Awake() {
-        
+public class Smoke : MonoBehaviour {
+
+    private int depth = 8;
+    void Start() {
+        name = "Smoke " + depth;
+
+        if (depth <= 1) {
+            return;
+        }
+        StartCoroutine(CreateAfterASecond());        
+    }
+
+    IEnumerator CreateAfterASecond() {
+        yield return new WaitForSeconds(1.5f);
+        Smoke childA = CreateChild(Vector3.up, Quaternion.identity);
+        Smoke childB = CreateChild(Vector3.right, Quaternion.Euler(0f, 0f, -90f));
+        Smoke childC = CreateChild(Vector3.left, Quaternion.Euler(0f, 0f, 90f));
+        Smoke childD = CreateChild(Vector3.down, Quaternion.identity);
+
+        childA.transform.SetParent(transform, false);
+        childB.transform.SetParent(transform, false);
+        childC.transform.SetParent(transform, false);
+        childD.transform.SetParent(transform, false);
+    }
+
+    Smoke CreateChild(Vector3 direction, Quaternion rotation) {
+        Smoke child = Instantiate(this);
+        child.depth = depth - 1;
+        child.transform.localPosition = direction;
+        child.transform.localRotation = rotation;
+        return child;
     }
 
     private void OnTriggerStay2D(Collider2D collision) {
@@ -13,6 +40,46 @@ public class Smoke : MonoBehaviour
             if (collision.gameObject.CompareTag("Player")){
                 GameManager.PlayerDied();
             }
+            if (collision.gameObject.CompareTag("Smoke")) {
+                Destroy(gameObject);
+            }
         }
     }
 }
+
+/*
+ * private IEnumerator LeakInAllPossibledirections(Vector3Int newOrginAsInt) {
+        Vector3Int Up    = newOrginAsInt;
+        Vector3Int Down  = newOrginAsInt;
+        Vector3Int Left  = newOrginAsInt;
+        Vector3Int Right = newOrginAsInt;
+
+        while (true) {
+            yield return timeBetweenSpreading;
+            Up      += Vector3Int.up;
+            Down    += Vector3Int.down;
+            Left    += Vector3Int.left;
+            Right   += Vector3Int.right;
+
+            if (!smokeMap.HasTile(Up)) {
+                TileBase tile = Instantiate(smokeGroundTile);
+                smokeMap.SetTile(Up, tile);
+                Instantiate(prefabSmoke, Up, Quaternion.identity);
+            }
+            if (!smokeMap.HasTile(Down)) {
+                TileBase tile = Instantiate(smokeGroundTile);
+                smokeMap.SetTile(Down, tile);
+                Instantiate(prefabSmoke, Down, Quaternion.identity);
+            }
+            if (!smokeMap.HasTile(Left)) {
+                TileBase tile = Instantiate(smokeGroundTile);
+                smokeMap.SetTile(Left, tile);
+                Instantiate(prefabSmoke, Left, Quaternion.identity);
+            }
+            if (!smokeMap.HasTile(Right)) {
+                TileBase tile = Instantiate(smokeGroundTile);
+                smokeMap.SetTile(Right, tile);
+                Instantiate(prefabSmoke, Right, Quaternion.identity);
+            }
+        }
+    }*/
